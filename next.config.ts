8d3cf433ed8f827083d84b6a78a719590857b212
Anton Borisov <anton.borisov@optimax.dev>
2025-01-29 14:41:57 +0400

    tweak config

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
     output: "export",
+    assetPrefix: '/next-whatsapp/',
     basePath: "/next-whatsapp",
+    images: {
+        unoptimized: true,
+    }
 };
 
 export default nextConfig;
